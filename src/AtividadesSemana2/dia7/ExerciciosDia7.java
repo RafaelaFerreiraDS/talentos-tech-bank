b75e1018140fb2979b6cf20b3f50a9a5c2c978ae
RafaelaFerreiraDS <rafaela.ferreira5@fatec.sp.gov.br>
2021-07-06 22:16:48 -0300

    continuacao da resolucao dos exercicios

--- src/AtividadesSemana2/dia7/ExerciciosDia7.java
+++ src/AtividadesSemana2/dia7/ExerciciosDia7.java
@@ -36,8 +36,9 @@ public class ExerciciosDia7 {
     }
 
     public static void exercicio2() {
-        /*Exercicio 3
-
+        /* Exercicio 2
+         * Escreva um algoritmo que leia números, insira em um array e após mostre a
+         * quantidade de números negativos.
          */
         int contador = 0;
         Scanner entrada = new Scanner(System.in);
@@ -62,7 +63,10 @@ public class ExerciciosDia7 {
     }
 
     public static void exercicio2Array() {
-
+        /* Exercicio 2
+         * Escreva um algoritmo que leia números, insira em um array e após mostre a
+         * quantidade de números negativos.
+         */
         Scanner entrada = new Scanner(System.in);
 
         int vetor[] = new int[5];
@@ -83,8 +87,8 @@ public class ExerciciosDia7 {
     }
 
     public static void exercicio3List() {
-        /*Exercicio 3
-
+        /* Exercicio 3
+         * Escreva um algoritmo que leia números, insira em um array e após mostre a quantidade de números pares.
          */
         int contador = 0;
         Scanner entrada = new Scanner(System.in);
@@ -106,8 +110,8 @@ public class ExerciciosDia7 {
     }
 
     public static void exercicio3Array() {
-        /*Exercicio 3
-
+        /* Exercicio 3
+         * Escreva um algoritmo que leia números, insira em um array e após mostre a quantidade de números pares.
          */
         Scanner entrada = new Scanner(System.in);
 
@@ -129,8 +133,8 @@ public class ExerciciosDia7 {
     }
 
     public static void exercicio4List() {
-        /*Exercicio 4
-
+        /* Exercicio 4
+         * Escreva um algoritmo que leia números, insira em um array e após mostre o maior valor.
          */
         Scanner entrada = new Scanner(System.in);
         List<Integer> lista = new ArrayList<>();
@@ -149,8 +153,8 @@ public class ExerciciosDia7 {
     }
 
     public static void exercicio4Array() {
-        /*Exercicio 4
-
+        /* Exercicio 4
+         * Escreva um algoritmo que leia números, insira em um array e após mostre o maior valor.
          */
         Scanner entrada = new Scanner(System.in);
 
@@ -162,29 +166,75 @@ public class ExerciciosDia7 {
         }
 
         Arrays.sort(vetor);
-        System.out.println("Maior número digitado: " + vetor[vetor.length-1]);
+        System.out.println("Maior número digitado: " + vetor[vetor.length - 1]);
 
         entrada.close();
 
     }
 
     public static void exercicio5() {
-        /*Exercicio 5
-
+        /* Exercicio 5
+         * Escreva um algoritmo que simula um jogo da forca simples. o usuario precisara adivinhar
+         * uma palavra chutando cada letra em no máximo 10 chutes, caso o usuario acerte a letra o
+         * jogo dirá que ele acertou, caso tenha errado, o numero de tentativas vai diminuindo.
+         * Caso o numero de tentativas chegue a 0 o usuário perde.
          */
-        Scanner entrada = new Scanner(System.in);
 
-        char vetor[] = "banana".toCharArray();
+        Scanner entrada = new Scanner(System.in).useDelimiter("\n");
 
-        String letra = entrada.next();
+        System.out.println("jogador 1, escolha a palavra que deverá ser adivinhada: ");
+        String palavraParaAdivinharInput = entrada.next();
 
-        for (int i = 0; i< vetor.length; i++){
-            if( vetor[i].e)
+        System.out.println("Jogador 2, de uma dica sobre a palavra a ser adivinhada: ");
+        String dicaDaPalavra = entrada.next();
+
+        int tamanhoDaPalavra = palavraParaAdivinharInput.trim().length();
+        int maxChute = 10;
+
+        char[] palavraParaAdivinhar = palavraParaAdivinharInput.toCharArray();
+
+        Boolean[] letrasCertas = new Boolean[palavraParaAdivinhar.length];
+        List<Boolean> listaLetrasCertas = new ArrayList<>();
+
+        for (char x : palavraParaAdivinhar) {
+            listaLetrasCertas.add(false);
         }
 
+        System.out.println("Vez do jogador 2:");
 
+        while (maxChute > 0 && Arrays.asList(letrasCertas).contains(false)) {
+            System.out.println("jogador 2, chute uma letra");
+            char chute = entrada.next().charAt(0);
 
+            boolean errouChute = true;
 
+            for (int i = 0; i < palavraParaAdivinhar.length; i++) {
+                if (chute == palavraParaAdivinhar[i]) {
+                    letrasCertas[i] = true;
+                    errouChute = false;
+                }
+            }
 
+            if (errouChute) {
+                maxChute--;
+                System.out.printf("Agora voce só tem %s chutes\n", maxChute);
+            }
+
+            for (int i = 0; i < palavraParaAdivinhar.length; i++) {
+                if (listaLetrasCertas.get(i)) {
+                    System.out.printf(" %s ", palavraParaAdivinhar[i]);
+                } else {
+                    System.out.println(" - ");
+                }
+
+            }
+        }
+
+        if (!listaLetrasCertas.contains(false)) {
+            System.out.println("Jogador 2 ganhou o jogo");
+        } else {
+            System.out.println("jogador 1 ganhou");
+        }
+        entrada.close();
     }
 }
