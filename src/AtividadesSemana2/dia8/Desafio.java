4e3ffa037da372d2ddf345589f77c3b3c25737ad
RafaelaFerreiraDS <rafaela.ferreira5@fatec.sp.gov.br>
2021-07-07 18:41:05 -0300

    desafio proposto em aula

---
+++ src/AtividadesSemana2/dia8/Desafio.java
@@ -0,0 +1,24 @@
+package AtividadesSemana2.dia8;
+
+public class Desafio {
+    public static void main(String[] args) {
+        /* Exercicio extra
+         * Dado um array de números inteiros com valores negativos e positivos,
+         * encontro o número mais próximo de zero. Se ouver valores como 2 e -2,
+         * considere o número positivo.
+         */
+        int vetor[] = {-4, 6, 9, 23, -5, 1, 65, 5, -15, 89, 4, -18, -1};
+
+        int numMaisPerto = vetor[0];
+
+        for (int i : vetor) {
+            if (Math.abs(numMaisPerto) > Math.abs(i)) {
+                numMaisPerto = i;
+            } else if (Math.abs(numMaisPerto) == Math.abs(i) && numMaisPerto < 0) {
+                numMaisPerto = i;
+            }
+        }
+
+        System.out.printf("Número mais perto de zero: %d",numMaisPerto);
+    }
+}
