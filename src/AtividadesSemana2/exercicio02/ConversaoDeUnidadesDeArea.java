2811ad0b45f7003a4292434e1b1479ab7aae954e
RafaelaFerreiraDS <rafaela.ferreira5@fatec.sp.gov.br>
2021-07-10 14:10:26 -0300

    exercicio fixacao static

---
+++ src/AtividadesSemana2/exercicio02/ConversaoDeUnidadesDeArea.java
@@ -0,0 +1,25 @@
+package AtividadesSemana2.exercicio02;
+
+public class ConversaoDeUnidadesDeArea {
+
+    public static final double UM_METRO_QUADRADO_EM_PES_QUADRADOS = 10.76;
+    public static final double UM_PE_QUADRADO_EM_CENTIMETROS_QUADRADOS = 929;
+    public static final double UMA_MILHA_QUADRADA_EM_ACRES = 640;
+    public static final double UM_ACRE_EM_PES_QUADRADOS = 43560;
+
+    public static double umMetroQuadradoEmPesQuadrados(double medida) {
+        return medida * UM_METRO_QUADRADO_EM_PES_QUADRADOS;
+    }
+
+    public static double umPeQuadradoEmCentimetrosQuadrados(double medida) {
+        return medida * UM_PE_QUADRADO_EM_CENTIMETROS_QUADRADOS;
+    }
+
+    public static double umaMilhaQuadradaEmAcres(double medida) {
+        return medida * UMA_MILHA_QUADRADA_EM_ACRES;
+    }
+
+    public static double umAcreEmPesQuadrados(double medida) {
+        return medida * UM_ACRE_EM_PES_QUADRADOS;
+    }
+}
---
+++ src/AtividadesSemana2/exercicio02/Main.java
@@ -0,0 +1,25 @@
+package AtividadesSemana2.exercicio02;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("------ Acre em pes quadrados ------");
+        System.out.println(ConversaoDeUnidadesDeArea.umAcreEmPesQuadrados(2));
+        System.out.println(ConversaoDeUnidadesDeArea.umAcreEmPesQuadrados(0.5));
+
+        System.out.println("------ Milha quadrada em acres ------");
+        System.out.println(ConversaoDeUnidadesDeArea.umaMilhaQuadradaEmAcres(2));
+        System.out.println(ConversaoDeUnidadesDeArea.umaMilhaQuadradaEmAcres(0.5));
+
+        System.out.println("------ Metro quadrado em pes ------");
+        System.out.println(ConversaoDeUnidadesDeArea.umMetroQuadradoEmPesQuadrados(2));
+        System.out.println(ConversaoDeUnidadesDeArea.umMetroQuadradoEmPesQuadrados(0.5));
+
+        System.out.println("------ Pe em centimetros ------");
+        System.out.println(ConversaoDeUnidadesDeArea.umPeQuadradoEmCentimetrosQuadrados(2));
+        System.out.println(ConversaoDeUnidadesDeArea.umPeQuadradoEmCentimetrosQuadrados(0.5));
+
+
+    }
+}
