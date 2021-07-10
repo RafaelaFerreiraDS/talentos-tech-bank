c1f28f9d7cf0e69548c0f6a1be657cb67724e4ed
RafaelaFerreiraDS <rafaela.ferreira5@fatec.sp.gov.br>
2021-07-10 11:33:23 -0300

    exercicio para aplicar conceitos de classe e encapsulamento

---
+++ src/AtividadesSemana2/dia12/universidade/exercicio01/ControleRemoto.java
@@ -0,0 +1,73 @@
+package AtividadesSemana2.dia12.universidade.exercicio01;
+
+public class ControleRemoto {
+
+    private Televisao televisao;
+
+    public void aumentarVolume() {
+        this.televisao.ajustarVolume(1);
+    }
+
+    public void diminuirVolume() {
+        this.televisao.ajustarVolume(-1);
+    }
+
+    public void consultarVolume() {
+        this.televisao.consultarVolume();
+    }
+
+    public void aumentarCanal() {
+        this.televisao.aumentarCanal();
+    }
+
+    public void diminuirCanal() {
+        this.televisao.diminuirCanal();
+    }
+
+    public void consultarCanal() {
+        this.televisao.consultarCanal();
+    }
+
+    public void trocarCanal(int novoCanal) {
+        this.televisao.trocarCanal(novoCanal);
+    }
+
+    public void informacoesTV() {
+        this.televisao.informacoesTV();
+    }
+
+    public Televisao getTelevisao() {
+        return televisao;
+    }
+
+    public void setTelevisao(Televisao televisao) {
+        this.televisao = televisao;
+    }
+
+    public static void main(String[] args) {
+
+        Televisao samgung = new Televisao();
+
+        ControleRemoto controleRemoto = new ControleRemoto();
+
+        controleRemoto.setTelevisao(samgung);
+
+        controleRemoto.aumentarVolume();
+        controleRemoto.aumentarCanal();
+
+        controleRemoto.aumentarCanal();
+
+        controleRemoto.informacoesTV();
+
+        controleRemoto.trocarCanal(99);
+
+        controleRemoto.aumentarCanal();
+        controleRemoto.informacoesTV();
+
+        controleRemoto.aumentarCanal();
+        controleRemoto.informacoesTV();
+
+
+
+    }
+}
---
+++ src/AtividadesSemana2/dia12/universidade/exercicio01/Televisao.java
@@ -0,0 +1,85 @@
+package AtividadesSemana2.dia12.universidade.exercicio01;
+
+public class Televisao {
+
+    private int volume = 15;
+    private int canal = 5;
+
+
+    public void ajustarVolume(Integer volume) {
+        if (volume == -1) {
+            this.diminuirVolume();
+        } else if (volume == 1) {
+            this.aumentarVolume();
+        }
+    }
+
+
+    private void aumentarVolume() {
+        if (getVolume() <= 100) {
+            this.setVolume(getVolume() + 1);
+        } else if (volume == 100) {
+            System.out.println("Volume está no máximo");
+        }
+    }
+
+    private void diminuirVolume() {
+        if (getVolume() > 0) {
+            this.setVolume(getVolume() - 1);
+        } else if (getVolume() == 0) {
+            System.out.println("Volume está no mínimo");
+        }
+    }
+
+    public void consultarVolume() {
+        this.getVolume();
+    }
+
+    public void aumentarCanal() {
+        if (this.canal == 100) {
+            this.canal = 1;
+        } else {
+            this.canal++;
+        }
+    }
+
+    public void diminuirCanal() {
+        if (this.canal == 1) {
+            this.canal = 100;
+        } else {
+            this.canal--;
+        }
+    }
+
+    public void consultarCanal() {
+        this.getCanal();
+    }
+
+    public void trocarCanal(int novoCanal) {
+        if (canal < 1 || canal > 100) {
+            return;
+        }
+        this.setCanal(novoCanal);
+    }
+
+    public void informacoesTV() {
+        System.out.println("Volume: " + getVolume());
+        System.out.println("Canal: " + getCanal());
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int canal) {
+        this.canal = canal;
+    }
+}
